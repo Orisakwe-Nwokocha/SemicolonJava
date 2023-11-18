7ccfe3d313c3064827fc1db13d3a096c6e79e01a
Orisha <orisakwenwokocha1@gmail.com>
2023-11-18 10:47:07 +0100

    Update Bad.java

--- Bad.java
+++ Bad.java
@@ -5,8 +5,8 @@ public class Bad {
 	Scanner input = new Scanner(System.in);
 	
 	int even = 0; 
-	made changes int odd = 0;
-	made changes;
+	made changes here int odd = 0;
+	made changes here;
 System.out.print("How many numbers (nElements) do you want to enter? ");
 	int num = input.nextInt();
 	
