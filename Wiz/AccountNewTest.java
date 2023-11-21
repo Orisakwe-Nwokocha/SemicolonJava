ec68ce97ee48e8f06548d4df6c1b4fb94e6c768b
Orisha <orisakwenwokocha1@gmail.com>
2023-11-21 20:37:35 +0100

    Add files via upload

---
+++ Wiz/AccountNewTest.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+public class AccountNewTest {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Welcome to Mavericks Bank");
+	
+	AccountNew account = new AccountNew();
+
+	System.out.printf("%nEnter your id: ");
+	int userId = input.nextInt();
+	account.setId(userId);
+
+	System.out.printf("%nEnter your initial balance: ");
+	double initialBalance = input.nextDouble();
+	account.setBalance(initialBalance);
+
+	System.out.printf("%nEnter annual interest rate: ");
+	double annualIntRate = input.nextDouble();
+	account.setAnnualInterestRate(annualIntRate);
+
+	System.out.printf("%nEnter date created: ");
+	String dateOfCreation = input.next();	
+ 
+	
+	
+	System.out.printf("%nWould you like to withdraw: ");
+	String prompt = input.next();
+		if (prompt.equals("yes")) {
+			System.out.printf("%nEnter withdraw amount: ");
+			double withdrawAmount = input.nextDouble();
+			account.withdraw(withdrawAmount);
+			
+			System.out.printf("%nWould you like to deposit: ");
+			String prompt2 = input.next();
+			
+			if (prompt2.equals("yes")) {
+				System.out.printf("%nEnter depsoit amount: ");
+				double depAmount = input.nextDouble();
+				account.deposit(depAmount);
+			
+				System.out.printf("%nYour new balance: %.3f%n", account.getBalance());
+	
+				System.out.printf("%nThank you for banking with us!!!%n");
+
+				System.out.printf("%nYour monthly interest is: %.3f%n", account.getMonthlyInterest());
+			}
+			
+			if (prompt2.equals("no")){
+				System.out.printf("%nYour new balance: %.3f%n", account.getBalance());
+	
+				System.out.printf("%nThank you for banking with us!!!%n");
+
+				System.out.printf("%nYour monthly interest is: %.3f%n", account.getMonthlyInterest());
+				}
+		}
+		
+		if (prompt.equals("no")) {
+			System.out.printf("%nWould you like to deposit: ");
+			String prompt3 = input.next();
+			
+			if (prompt3.equals("yes")) {
+				System.out.printf("%nEnter depsoit amount: ");
+				double depAmount = input.nextDouble();
+				account.deposit(depAmount);
+			
+				System.out.printf("%nYour new balance: %.3f%n", account.getBalance());
+	
+				System.out.printf("%nThank you for banking with us!!!%n");
+
+				System.out.printf("%nYour monthly interest is: %.3f%n", account.getMonthlyInterest());
+
+			}
+
+			if (prompt3.equals("no")) {
+			System.out.printf("%nYour new balance: %.3f%n", account.getBalance());
+	
+			System.out.printf("%nThank you for banking with us!!!%n");
+
+			System.out.printf("%nYour monthly interest is: %.3f%n", account.getMonthlyInterest());
+			}
+		}
+		
+	
+
+
+
+
+
+
+
+	
+
+
+	}
+}
