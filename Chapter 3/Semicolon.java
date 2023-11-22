3be6f6ebbedb1cd208dc629d3993a7a04f7d849c
Orisha <orisakwenwokocha1@gmail.com>
2023-11-22 06:59:48 +0100

    Add files via upload

---
+++ Chapter 3/Semicolon.java
@@ -0,0 +1,31 @@
+public class Semicolon {
+	private int numberOfStudent;
+	private int numberOfCohort;	
+	
+
+	public Semicolon(int numberOfStudent, int numberOfCohort) {
+		this.numberOfStudent = numberOfStudent;
+		this.numberOfCohort = numberOfCohort;			
+	}
+	
+	public void setNumberOfStudent(int numberOfStudent) {
+		this.numberOfStudent = numberOfStudent;
+	}
+	
+	public int getNumberOfStudent() {
+		return numberOfStudent;
+	}
+
+	public void setNumberOfCohort(int numberOfCohort) {
+		this.numberOfCohort = numberOfCohort;
+	}
+	
+	public int getNumberOfCohort() {
+		return numberOfCohort;
+	}
+
+	
+
+
+ 	
+}
---
+++ Chapter 3/SemicolonTest.java
@@ -0,0 +1,14 @@
+public class SemicolonTest {
+	public static void main(String[] args) {
+
+	Semicolon semicolon = new Semicolon(1500, 19);
+
+ 
+	System.out.printf("Number of students = %d%n", semicolon.getNumberOfStudent());
+	System.out.printf("Number of cohorts = %d%n", semicolon.getNumberOfCohort());
+	
+
+	
+
+	}
+}
