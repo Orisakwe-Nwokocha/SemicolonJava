51a2c1d64ecc4ed6dae0dd1c8b677fb546e97cb3
Orisha <orisakwenwokocha1@gmail.com>
2023-12-04 09:49:36 +0100

    Add files via upload

---
+++ Chapter 5/TwelveDaysOfChristmas.java
@@ -0,0 +1,94 @@
+public class TwelveDaysOfChristmas {
+
+   public static void main(String[] args) {
+
+	for (int i = 1; i <= 12; i++) {
+
+		switch (i) {
+			case 1: 
+				System.out.print("On the first day");
+				break;
+			case 2: 
+				System.out.print("On the second day");
+				break;
+			case 3: 
+				System.out.print("On the third day");
+				break;
+			case 4: 
+				System.out.print("On the fourth day");
+				break;
+			case 5: 
+				System.out.print("On the fifth day");
+				break;
+			case 6: 
+				System.out.print("On the sixth day");
+				break;
+			case 7: 
+				System.out.print("On the seventh day");
+				break;
+			case 8: 
+				System.out.print("On the eighth day");
+				break;
+			case 9: 
+				System.out.print("On the ninth day");
+				break;
+			case 10: 
+				System.out.print("On the tenth day");
+				break;
+			case 11: 
+				System.out.print("On the eleventh day");
+				break;
+			case 12: 
+				System.out.print("On the twelfth day");
+				break;
+		}
+
+		switch (i) {
+			case 1: 
+				System.out.printf(" of Christmas my true love sent to me%nA partridge in a pear tree%n%n");
+				break;
+			case 2: 
+				System.out.printf(" of Christmas my true love sent to me%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 3: 
+				System.out.printf(" of Christmas my true love sent to me%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 4: 
+				System.out.printf(" of Christmas my true love sent to me%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 5: 
+				System.out.printf(" of Christmas my true love sent to me%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 6: 
+				System.out.printf(" of Christmas my true love sent to me%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 7: 
+				System.out.printf(" of Christmas my true love sent to me%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 8: 
+				System.out.printf(" of Christmas my true love sent to me%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 9: 
+				System.out.printf(" of Christmas my true love sent to me%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 10: 
+				System.out.printf(" of Christmas my true love sent to me%nTen lords a-leaping,%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 11: 
+				System.out.printf(" of Christmas my true love sent to me%nEleven pipers piping,%nTen lords a-leaping,%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				break;
+			case 12: 
+				System.out.printf(" of Christmas my true love sent to me%nTwelve drummers drumming,%nEleven pipers piping,%nTen lords a-leaping,%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n"); 
+				break;
+		}
+
+
+	}
+
+   }
+
+}
+
+
+
+
