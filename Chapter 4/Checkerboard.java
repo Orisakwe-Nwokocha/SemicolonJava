e22ff39a16b20c83960b083a947330f4f84648fa
Orisha <orisakwenwokocha1@gmail.com>
2023-12-12 19:41:41 +0100

    Add files via upload

--- Chapter 4/Checkerboard.java
+++ Chapter 4/Checkerboard.java
@@ -1,58 +1,15 @@
 public class Checkerboard {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
- 	int i;
-	int j;
-	
-	for (i = 1; i <= 8; i++) { 
-    		System.out.print ("*" + " "); 
-	}	
-	
-	System.out.println(); 
-  	
-	 for (j = 0; j < i - 1; j++) { 
-    			System.out.print (" *" + ""); 
-  		}
-  			
-	System.out.println();	
-		
-	for (i = 1; i <= 8; i++) { 
-    		System.out.print ("*" + " "); 
-	}	
-	
-	System.out.println(); 
-  	
-	 for (j = 0; j < i - 1; j++) { 
-    			System.out.print (" *" + ""); 
-  		}
-  			
-	System.out.println();	
-		
-	for (i = 1; i <= 8; i++) { 
-    		System.out.print ("*" + " "); 
-	}	
-	
-	System.out.println(); 
-  	
-	 for (j = 0; j < i - 1; j++) { 
-    			System.out.print (" *" + ""); 
-  		}
-  			
-	System.out.println();	
-		
-	for (i = 1; i <= 8; i++) { 
-    		System.out.print ("*" + " "); 
-	}	
-	
-	System.out.println(); 
-  	
-	 for (j = 0; j < i - 1; j++) { 
-    			System.out.print (" *" + ""); 
-  		}
-  			
-	System.out.println();	
-		
-		
-		
-	} 
- }
+        for (int i = 1; i <= 8; i++) {
+            if (i % 2 == 0) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 8; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
