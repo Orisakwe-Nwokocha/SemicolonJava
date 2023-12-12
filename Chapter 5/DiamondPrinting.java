1777af8908e42762392ffc9e9ddd2245f22f2f49
Orisha <orisakwenwokocha1@gmail.com>
2023-12-12 17:23:20 +0100

    Update DiamondPrinting.java

--- Chapter 5/DiamondPrinting.java
+++ Chapter 5/DiamondPrinting.java
@@ -15,7 +15,7 @@ public class DiamondPrinting {
 	for (int i = 4; i >= 1; i--){
 		for (int s = 1; s <= 5- i; s++) {
 			System.out.print(" ");
-		}s
+		}
 		for (int j = 1; j <= 2 * i - 1; j++) {
 			System.out.print("*");
 		}
