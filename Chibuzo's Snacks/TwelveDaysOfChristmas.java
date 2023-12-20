5a2d5fb9c01001b840dfad14e05fa355c2019538
Orisha <orisakwenwokocha1@gmail.com>
2023-12-20 17:47:14 +0100

    Add files via upload

---
+++ Chibuzo's Snacks/TwelveDaysOfChristmas.java
@@ -0,0 +1,84 @@
+public class TwelveDaysOfChristmas {
+
+   public static void main(String[] args) {
+
+	for (int i = 1; i <= 12; i++) {
+
+		System.out.print("On the ");
+		christmasDay(i);
+		System.out.print(" day");
+
+		switch (i) {
+			case 1 -> System.out.printf(" of Christmas my true love sent to me%nA partridge in a pear tree%n%n");
+				
+			case 2 -> System.out.printf(" of Christmas my true love sent to me%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 3 -> System.out.printf(" of Christmas my true love sent to me%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 4 -> System.out.printf(" of Christmas my true love sent to me%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 5 -> System.out.printf(" of Christmas my true love sent to me%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 6 -> System.out.printf(" of Christmas my true love sent to me%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 7 -> System.out.printf(" of Christmas my true love sent to me%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 8 -> System.out.printf(" of Christmas my true love sent to me%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 9 -> System.out.printf(" of Christmas my true love sent to me%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 10 -> System.out.printf(" of Christmas my true love sent to me%nTen lords a-leaping,%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 11 -> System.out.printf(" of Christmas my true love sent to me%nEleven pipers piping,%nTen lords a-leaping,%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n");
+				
+			case 12 -> System.out.printf(" of Christmas my true love sent to me%nTwelve drummers drumming,%nEleven pipers piping,%nTen lords a-leaping,%nNine ladies dancing,%nEight maids a-milking,%nSeven swans a-swimming,%nSix geese a-laying,%nFive golden rings,%nFour calling birds,%nThree French hens,%nTwo turtle doves,%nAnd a partridge in a pear tree%n%n"); 
+				
+		}
+
+
+	}
+
+
+   }
+
+	public static void christmasDay(int day) {
+	
+	switch (day) {
+			case 1 -> System.out.print("first");
+				
+			case 2 -> System.out.print("second");
+				
+			case 3 -> System.out.print("third");
+				
+			case 4 -> System.out.print("fourth");
+				
+			case 5 -> System.out.print("fifth");
+				
+			case 6 -> System.out.print("sixth");
+				
+			case 7 -> System.out.print("seventh");
+				
+			case 8 -> System.out.print("eighth");
+				
+			case 9 -> System.out.print("ninth");
+				
+			case 10 -> System.out.print("tenth");
+				
+			case 11 -> System.out.print("eleventh");
+				
+			case 12 -> System.out.print("twelfth");
+				
+		}
+	
+		
+
+	}
+	
+
+
+
+}
+
+
+
+
