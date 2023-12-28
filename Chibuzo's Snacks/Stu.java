5bcd71055041f314fe227a4a4f49bf9a90863a00
Orisha <orisakwenwokocha1@gmail.com>
2023-12-28 11:11:22 +0100

    Add files via upload

---
+++ Chibuzo's Snacks/Stu.java
@@ -0,0 +1,12 @@
+public class Stu {
+    public static void main(String[] args) {
+        int[][] h = {{4, 32}, {89, 23}, {45, 67}, {23, 34}, {12, 98}};
+
+        StudentGrade.setGrades(h);
+
+        StudentGrade.processGrades();
+
+    }
+
+
+}
