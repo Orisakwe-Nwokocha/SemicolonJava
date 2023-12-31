6e68fe8a4d7db25279427fd4a60a2d7fdd1bbcfb
Orisha <orisakwenwokocha1@gmail.com>
2023-12-31 19:47:06 +0100

    Add files via upload

---
+++ Chibuzo's Snacks/MBTIPersonalityTestApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class MBTIPersonalityTestApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your name:");
+        String name = input.nextLine();
+
+        MBTIPersonalityTest.displayQuestions();
+
+        System.out.printf("%nHello %s, you selected%n", name);
+
+        MBTIPersonalityTest.extrovertedVsIntroverted();
+        MBTIPersonalityTest.sensingVsIntuitive();
+        MBTIPersonalityTest.thinkingVsFeeling();
+        MBTIPersonalityTest.judgingVsPerceptive();
+
+        MBTIPersonalityTest.getMyAnswer();
+
+
+
+    }
+
+}
