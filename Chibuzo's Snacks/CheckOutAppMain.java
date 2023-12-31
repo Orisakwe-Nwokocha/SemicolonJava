1e36bc22a70837f9a472656b23781db8b9440c36
Orisha <orisakwenwokocha1@gmail.com>
2023-12-31 19:46:21 +0100

    Added a main method for CheckOutApp.java

---
+++ Chibuzo's Snacks/CheckOutAppMain.java
@@ -0,0 +1,9 @@
+public class CheckOutAppMain {
+    public static void main(String[] args) {
+        CheckOutApp customer1 = new CheckOutApp();
+        CheckOutApp customer2 = new CheckOutApp();
+
+        customer1.startApp();
+        customer2.startApp();
+    }
+}
