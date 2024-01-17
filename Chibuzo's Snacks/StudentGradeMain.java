535d70539b7a6cadccae48c2af76afff8f5e1730
Orisha <orisakwenwokocha1@gmail.com>
2024-01-17 06:55:24 +0100

    Update and rename Stu.java to StudentGradeMain.java

---
+++ Chibuzo's Snacks/StudentGradeMain.java
@@ -0,0 +1,13 @@
+public class StudentGradeMain {
+    public static void main(String[] args) {
+       // int[][] h = {{4, 32}, {89, 23}, {45, 67}, {23, 34}, {12, 98}};
+
+       // StudentGrade.setGrades(h);
+
+        StudentGrade.inputGrades();
+        StudentGrade.processGrades();
+
+    }
+
+
+}
