84258483881b7da20ee3393f10be9345863deb83
Orisha <orisakwenwokocha1@gmail.com>
2024-01-19 00:40:00 +0100

    Update SumOfTwoNumbersInArray.java

--- Pet Projects/SumOfTwoNumbersInArray.java
+++ Pet Projects/SumOfTwoNumbersInArray.java
@@ -2,20 +2,25 @@ import java.util.Arrays;
 
 public class SumOfTwoNumbersInArray {
     public static void twoSum(int[] nums, int target) {
-
 	int[] newArr = new int[nums.length];
 	int[] newArray = new int[2];
 
-
-	for (int index = 0; index < nums.length - 1; index++) {
-        	if (nums[index] + nums[index + 1] == target) {
+	boolean condition = false;
+      
+        for (int i = 0; i < nums.length; i++) {
+            for (int index = 1; index < nums.length; index++) {
+                if (nums[i] + nums[index] == target && i != index) {
 		    newArr[index] = nums[index];
 		    newArr[index + 1] = nums[index + 1];
-
-		    newArray[0] = index;
-		    newArray[1] = index + 1;
-		}
-	}
+			
+                    newArray[0] = i;
+                    newArray[1] = index;
+			
+                    condition = true;	      	
+           	}
+            }
+	    if (condition) break;
+       }
 
 	System.out.println(Arrays.toString(newArr));
 	System.out.println(Arrays.toString(newArray));
@@ -30,5 +35,11 @@ public class SumOfTwoNumbersInArray {
 
 	int[] c = {3,3};
 	twoSum(c, 6);
+
+	int[] d = {3,2,3};
+	twoSum(d, 6);
+	    
+	int[] e = {1,3,4,2};
+	twoSum(e, 6);
     }
 }
