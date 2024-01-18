14a5b3d02394611976616e2cddb9a47a814bb801
Orisha <orisakwenwokocha1@gmail.com>
2024-01-18 11:26:01 +0100

    Update ProductOfTwoIntegers.java

--- Mr Femi's Snacks/ProductOfTwoIntegers.java
+++ Mr Femi's Snacks/ProductOfTwoIntegers.java
@@ -1,6 +1,6 @@
 public class ProductOfTwoIntegers {
     public static void main(String[] args) {
-    	productOfTwoIntegers(-4, 5);
+    	productOfTwoIntegers(4, 5);
     }
 
     public static void productOfTwoIntegers(int number1, int number2) {
@@ -19,7 +19,7 @@ public class ProductOfTwoIntegers {
 	for (int count = 1; count <= number2; count++) {
 		product += number1;
     }
-	if (condition2 < 0 && condition2 < 0) System.out.printf("The product of -%d and -%d is: %d", number1, number2, product);
+	if (condition1 < 0 && condition2 < 0) System.out.printf("The product of -%d and -%d is: %d", number1, number2, product);
 	else if (condition1 < 0) System.out.printf("The product of -%d and %d is: -%d", number1, number2, product);
     	else if (condition2 < 0) System.out.printf("The product of %d and -%d is: -%d", number1, number2, product);
 	else System.out.printf("The product of %d and %d is: %d", number1, number2, product);
