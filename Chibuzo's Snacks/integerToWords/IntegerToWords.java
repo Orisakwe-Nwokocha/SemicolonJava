117fc35350ee5b0c4279ffe5631486b286e12cb7
Orisha <orisakwenwokocha1@gmail.com>
2024-02-08 22:51:09 +0100

    finalised time in words conversion function

--- Chibuzo's Snacks/integerToWords/IntegerToWords.java
+++ Chibuzo's Snacks/integerToWords/IntegerToWords.java
@@ -2,60 +2,65 @@ package integerToWords;
 
 public class IntegerToWords {
     public static String getHourInWords(int hour) {
-        return ab(hour);
-
+        return convertIntegerToString(hour);
     }
 
-    private static String ab(int hour) {
-        return switch (hour) {
-            case 1 -> "One";
-            case 2 -> "Two";
-            case 3 -> "Three";
-            case 4 -> "Four";
-            case 5 -> "Five";
-            case 6 -> "Six";
-            case 7 -> "Seven";
-            case 8 -> "Eight";
-            case 9 -> "Nine";
-            case 10 -> "Ten";
-            case 11 -> "Eleven";
-            case 12 -> "Twelve";
-            default -> throw new IllegalArgumentException("Hour must be between 1 and 12");
-        };
+    public static String getMinuteInWords(int minute) {
+        return convertIntegerToString(minute);
     }
 
+    private static String convertIntegerToString(int number) {
+        return getOutput(number);
+    }
 
-
-    public static String convertIntegerToString(int minute) {
-        String[] minuteInWords = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+    private static String getOutput(int number) {
+        String[] numbersInWords = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
                 "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
                 "Nineteen", "Twenty", "Thirty", "Forty", "Fifty"};
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50};
 
         String output = "";
 
-        for (int count = 0; count < 2; count++) {
+        for (int index = numbers.length - 1; index >= 0; index--) {
+            boolean numberIsWithinRange = number > 19 && number % 10 != 0;
 
-            for (int index = numbers.length - 1; index > 0; index--) {
-                if (minute >= numbers[index]) {
-                    output += minuteInWords[index] + " ";
-                    minute -= numbers[index];
-                }
+            if (numberIsWithinRange && number >= numbers[index]) {
+                output += numbersInWords[index] + " ";
+                number -= numbers[index];
+            }
+            else if (number == numbers[index]) {
+                output += numbersInWords[index];
+                number -= numbers[index];
             }
         }
 
         return output;
-
     }
 
-/*    boolean condition = minute > 19;
+    public static String displayTimeInWords(int hour, int minute) {
+        if (timeIsNotValid(hour, minute)) throw new IllegalArgumentException("Hour and/or minute was out of range");
 
-                if (condition && minute >= numbers[index]) {
-        output += minuteInWords[index] + "-";
-        minute -= numbers[index];
+        hour = hourFormatter(hour);
+
+        if (minute == 0) return getHourInWords(hour) + " o'clock";
+        else if (minute == 15) return "quarter past " + getHourInWords(hour);
+        else if (minute < 30) return getMinuteInWords(minute) + " minutes past " + getHourInWords(hour);
+        else if (minute == 30) return "half past " + getHourInWords(hour);
+        else if (minute == 45) return "quarter to " + getHourInWords(hour == 12 ? 1 : hour + 1);
+
+        return getMinuteInWords(60 - minute) + " minutes to " + getHourInWords(hour == 12 ? 1 : hour + 1);
     }
-                else if (minute == numbers[index]) {
-        output += minuteInWords[index];
-        minute -= numbers[index];
-    }*/
+
+    private static boolean timeIsNotValid(int hour, int minute) {
+        if (hour < 0 || hour > 23) return true;
+
+        return minute < 0 || minute > 59;
+    }
+
+    private static int hourFormatter(int hour) {
+        if (hour > 12) return hour - 12;
+        else if (hour == 0) return 12;
+
+        return hour;
+    }
 }
---
+++ Chibuzo's Snacks/integerToWords/IntegerToWordsTest.java
@@ -0,0 +1,35 @@
+package integerToWords;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToWordsTest {
+
+    @Test
+    public void hourInIntegerIsInputted_returnsHourInWordsTest() {
+        assertEquals("Eleven", IntegerToWords.getHourInWords(11));
+    }
+
+
+    @Test
+    public void minuteInIntegerIsInputted_returnsMinuteInWordsTest() {
+        assertEquals("Forty Five", IntegerToWords.getMinuteInWords(45));
+    }
+
+    @Test
+    public void inputTimeInInteger_returnsTimeInWordsTest() {
+        assertEquals("Fourteen minutes to One", IntegerToWords.displayTimeInWords(12, 46));
+        assertEquals("Eight minutes past Eleven", IntegerToWords.displayTimeInWords(23, 8));
+        assertEquals("quarter to One", IntegerToWords.displayTimeInWords(0, 45));
+    }
+
+    @Test
+    public void inputInvalidTime_throwsExceptionTest() {
+        assertThrows(IllegalArgumentException.class, ()->IntegerToWords.displayTimeInWords(24, -1));
+        assertThrows(IllegalArgumentException.class, ()->IntegerToWords.displayTimeInWords(-1, 60));
+        assertThrows(IllegalArgumentException.class, ()->IntegerToWords.displayTimeInWords(11, 78));
+        assertThrows(IllegalArgumentException.class, ()->IntegerToWords.displayTimeInWords(37, 30));
+    }
+}
