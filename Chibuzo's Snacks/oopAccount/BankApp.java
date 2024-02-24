93b19d3aec9dfcf0195381e3a95cf56703227d50
Orisha <orisakwenwokocha1@gmail.com>
2024-02-24 12:27:44 +0100

    bank app version 1.0

--- Chibuzo's Snacks/oopAccount/BankApp.java
+++ Chibuzo's Snacks/oopAccount/BankApp.java
@@ -63,7 +63,7 @@ public class BankApp {
     private static void exitApp() {
         try {
             print("exiting...");
-            Thread.sleep(2000);
+            Thread.sleep(1500);
         }
         catch (InterruptedException ignored) {
         }
