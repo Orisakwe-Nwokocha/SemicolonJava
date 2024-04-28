641e66b63f4fb98a1468e19fa3ac2a0f4980e558
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-04-29 00:16:16 +0100

    deleted a file and added cipher file

---
+++ Pet Projects/Cipher.java
@@ -0,0 +1,32 @@
+package mrFemi;
+
+public class Cipher {
+    public static void main(String[] args) {
+        System.out.println(cipher("SERR PBQR PNZC"));
+        System.out.println(cipher("SeRr PBQR PNZC"));
+        System.out.println(cipher("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
+
+    }
+
+    public static String cipher(String text) {
+        final int SHIFT = 13;
+        StringBuilder cipher = new StringBuilder();
+
+        for (char character : text.toCharArray()) {
+            if (!Character.isLetter(character)) {
+                cipher.append(character);
+                continue;
+            }
+
+            int codePoint = character;
+            boolean isAfterM = (Character.isLowerCase(character) && character > 'm') || (Character.isUpperCase(character) && character > 'M');
+            if (isAfterM) codePoint -= SHIFT;
+            else codePoint += SHIFT;
+
+            cipher.appendCodePoint(codePoint);
+        }
+
+        return cipher.toString();
+    }
+
+}
