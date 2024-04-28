659ce3f5d5dc21b10daa68f423c5f47dde623f8b
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-04-28 18:25:34 +0100

    added print stars

--- Chapter 5/PrintStars.java
+++ Chapter 5/PrintStars.java
@@ -1,6 +1,11 @@
 public class PrintStars {
     public static void main(String[] args) {
+        printTriangles();
+        System.out.println();
+        printDiamond();
+    }
 
+    private static void printTriangles() {
         final int MAX_ROWS = 10;
         for (int counter = 1; counter <= MAX_ROWS; counter++) {
             printStars(counter);
@@ -12,7 +17,24 @@ public class PrintStars {
             printStars(counter);
             System.out.println();
         }
+    }
 
+    private static void printDiamond() {
+        int stars = 1;
+        int spaces = 4;
+        for (int counter = 1; counter <= 9; counter++) {
+            printSpaces(spaces);
+            printStars(stars);
+            System.out.println();
+            if (counter >= 5) {
+                stars -= 2;
+                spaces++;
+                continue;
+            }
+            stars += 2;
+            spaces--;
+
+        }
     }
 
     private static void printSpaces(int number) {
